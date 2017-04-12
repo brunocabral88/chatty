acacfe241175a893e789c7883e45712f9b319e64
Bruno Farias <bruno.cabral88@hotmail.com>
2017-04-12 14:54:49 -0400

    Moved footer functions to the App component for centralization and consistency of this.state

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ import Footer from './Footer.jsx'
 const appData = {
   currentUser: {name:null},
   messages: [],
-  users_connected: 1
+  users_connected: 1,
+  username: "Anonymous",
+  message: ''
 }
 
 class App extends Component {
@@ -36,12 +38,36 @@ class App extends Component {
     }
   }
 
+  // Footer methods
+  handleUserChange = (ev) => {
+		if (ev.target.value !== this.state.username) {
+			if (this.state.username == "Anonymous" && ev.target.value == "") {
+				return false;
+			}
+			this.sendMessageToSocket({ type: 'notification', content: `User ${this.state.username} changed their name to ${(ev.target.value || "Anonymous")}.` });
+			this.setState({ username: (ev.target.value || "Anonymous") });
+		}
+  }
+
+  handleTextChange = (ev) => {
+		if (ev.key === 'Enter' && this.state.content) {
+			this.sendMessageToSocket(this.state);
+			ev.target.value = '';
+			this.setState({content: null});
+		} else {
+			this.setState({content: ev.target.value, type: 'message', user_color: this.state.userColor })
+		}
+	}
+
   render() {
     return (
     	<div>
 	    	<Navbar usersConnected={this.state.users_connected} />
 	    	<MessagesBox messageList={this.state.messages} userColor={this.state.user_color} />
-	    	<Footer userColor={this.state.user_color} sendMessageToSocket={this.sendMessageToSocket} currentUser={this.state.currentUser} />
+	    	<Footer userColor={this.state.user_color} sendMessageToSocket={this.sendMessageToSocket} 
+          currentUser={this.state.currentUser} 
+          handleUserChange={this.handleUserChange}
+          handleTextChange={this.handleTextChange} />
 	    </div>
     )
   }
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,38 +1,14 @@
 import React,{Component} from 'react';
 
 class Footer extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			username: (this.props.currentUser.name || "Anonymous"),
-			message: ''
-		}
-	}
-	handleTextChange = (ev) => {
-		if (ev.key === 'Enter' && this.state.content) {
-			console.log(this.state);
-			this.props.sendMessageToSocket(this.state);
-			ev.target.value = '';
-			this.setState({content: null});
-		} else {
-			this.setState({content: ev.target.value, type: 'message', user_color: this.props.userColor })
-		}
-	}
 
-	handleUserChange = (ev) => {
-		if (ev.target.value !== this.state.username) {
-			if (this.state.username == "Anonymous" && ev.target.value == "") {
-				return false;
-			}
-			this.props.sendMessageToSocket({ type: 'notification', content: `User ${this.state.username} changed their name to ${(ev.target.value || "Anonymous")}.` });
-			this.setState({ username: (ev.target.value || "Anonymous") });
-		}
-	}
 	render() {
 		return(
 			<footer className={"chatbar"}>
-			  <input className={"chatbar-username"} onBlur={this.handleUserChange} placeholder={"Your Name (Optional)"} defaultValue={this.props.currentUser.name} />
-			  <input className={"chatbar-message"} onKeyUp={this.handleTextChange} placeholder={"Type a message and hit ENTER"} />
+			  <input className={"chatbar-username"} onBlur={this.props.handleUserChange} placeholder={"Your Name (Optional)"}
+				 	defaultValue={this.props.currentUser.name} />
+			  <input className={"chatbar-message"} onKeyUp={this.props.handleTextChange}
+					placeholder={"Type a message and hit ENTER"} />
 			</footer>
 		)
 	}
